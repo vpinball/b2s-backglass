c0f0f00211c7b1dcd7ee3ad163d1ffb940f987ae
toxie <toxie@ainc.de>
2017-12-29 17:36:44 +0000

    add missing files from rev 15 (aka d'oh!)

---
+++ b2sbackglassserverexe/b2sbackglassserverexe/Forms/Background.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.Windows.Forms
+
+Public Class Background
+
+#Region "constructor"
+
+    Public Sub New()
+
+        InitializeComponent()
+
+        ' set some styles
+        'Me.SetStyle(ControlStyles.AllPaintingInWmPaint Or ControlStyles.UserPaint Or ControlStyles.OptimizedDoubleBuffer, True)
+        Me.DoubleBuffered = True
+
+    End Sub
+
+#End Region
+
+End Class
