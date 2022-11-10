ce01e958bdc3ce381f34ad8a9ab904257438c331
Richard Ludwig <Richard.Ludwig+gh@gmail.com>
2022-11-10 19:27:33 +0100

    B2SBackglassServerRegisterApp:  Added registration of the B2S Context menu including small cmd to copy the file

---
+++ ScreenResTemplates/Full Screen.res
@@ -0,0 +1,28 @@
+# Playfield Screen resolution width/height
+1920
+1080
+# Backglass Screen resolution width/height
+1400
+908
+# Define Backglass using Display Devicename screen number (\\.\DISPLAY)x or screen coordinates (@x) or screen index (=x)
+2
+# x position for the backglass relative To the upper left corner Of the Playfield screen
+2440
+# y position for the backglass On the selected display (Normally left at 0)
+3
+# width/height Of the DMD area In pixels - For 3 screen setup
+426
+294
+# X/Y position Of the DMD area relative To the upper left corner of the backglass screen - For 3 screen setup
+2963
+906
+# Y-flip, flips the LED display upside down
+0
+# X/Y position pos When StartBackground Is active, relative To upper left corner Of Playfield ('Small' Button In the Options)
+0
+0
+# width/height Of the backglass When StartBackground Is active
+0
+0
+# C:\path\Frame (The path To the location where you have the background image)
+
--- b2sbackglassserverregisterapp/b2sbackglassserverregisterapp/formBackglassServerRegApp.vb
+++ b2sbackglassserverregisterapp/b2sbackglassserverregisterapp/formBackglassServerRegApp.vb
@@ -1,5 +1,5 @@
 ﻿Imports Microsoft.Win32
-Imports System.Security.Principal
+Imports System.IO
 
 Public Class formBackglassServerRegApp
 
@@ -8,68 +8,127 @@ Public Class formBackglassServerRegApp
 
         Me.Visible = False
 
-        'If Not CheckB2SServer(False) Then
-
-        ' base vars
         Dim basepath As String = String.Empty
         Dim regasmpath As String = String.Empty
         Dim version As String = String.Empty
+        Dim dialogResult As DialogResult
+
+        If CheckB2SServer(False) Then
+            dialogResult = MessageBox.Show("The 'B2S Server' is already registered." & vbCrLf & vbCrLf & "Do you REALLY want to (try to) reregister it???", My.Application.Info.AssemblyName, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+        Else
+            dialogResult = MessageBox.Show("The 'B2S Server' is not registered yet." & vbCrLf & vbCrLf & "Do you want to register it?", My.Application.Info.AssemblyName, MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+        End If
 
         ' get .NET framework base directoy
-        Dim regkey As RegistryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\.NetFramework", False)
-        If regkey IsNot Nothing Then
-            basepath = regkey.GetValue("InstallRoot")
-        End If
-        regkey.Close()
+        If dialogResult = DialogResult.Yes Then
+            Using regkey As RegistryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\.NetFramework", False)
+                If regkey IsNot Nothing Then
+                    basepath = regkey.GetValue("InstallRoot")
+                End If
+            End Using
 
-        ' get version info
-        If Not String.IsNullOrEmpty(basepath) Then
-            regkey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\.NetFramework\Policy\v4.0", False)
-            If regkey IsNot Nothing Then
-                version = "v4.0"
-                For Each valuename As String In regkey.GetValueNames()
-                    Dim path As String = IO.Path.Combine(basepath, version & "." & valuename)
-                    If IO.Directory.Exists(path) Then
-                        regasmpath = path
-                        Exit For
+            ' get version info
+            If Not String.IsNullOrEmpty(basepath) Then
+                Using regkey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\.NetFramework\Policy\v4.0", False)
+                    If regkey IsNot Nothing Then
+                        version = "v4.0"
+                        For Each valuename As String In regkey.GetValueNames()
+                            Dim path As String = IO.Path.Combine(basepath, version & "." & valuename)
+                            If IO.Directory.Exists(path) Then
+                                regasmpath = path
+                                Exit For
+                            End If
+                        Next
+                        regkey.Close()
+                        'Else
+                        '    regkey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\.NetFramework\policy\v2.0", False)
+                        '    If regkey IsNot Nothing Then version = "v2.0"; regkey.Close() End If
                     End If
-                Next
-                regkey.Close()
-            Else
-                '    regkey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\.NetFramework\policy\v2.0", False)
-                '    If regkey IsNot Nothing Then
-                '        version = "v2.0"
+                End Using
+            End If
 
-                '        regkey.Close()
-                '    End If
+            ' check whether everything is fine
+            If String.IsNullOrEmpty(basepath) Then
+                MessageBox.Show("Error, no basepath found.")
+            ElseIf String.IsNullOrEmpty(regasmpath) Then
+                MessageBox.Show("Error, no regasmpath found.")
+            Else
+                ' do the register operation
+                ShellAndWait(regasmpath)
+                ShellAndWait(regasmpath.Replace("\Framework\", "\Framework64\"))
+            End If
+            CheckB2SServer(True)
+        End If
+
+        dialogResult = MessageBox.Show("Do you want to (re-)register the context menu (Yes)" & vbCrLf & vbCrLf & "Unregister the context menu (No)" & vbCrLf & vbCrLf & "Or please just press cancel", My.Application.Info.AssemblyName, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information)
+
+        If (dialogResult = DialogResult.Yes) Or (dialogResult = DialogResult.No) Then
+            Dim rkReg As RegistryKey = Registry.ClassesRoot
+            Dim strKey As String = "Applications\VPinballX.exe\shell"
+
+            Dim pinEditValue As String = ""
+
+            Using openKey As RegistryKey = rkReg.OpenSubKey(strKey + "\edit\command", False)
+                If openKey IsNot Nothing Then
+                    Try
+                        pinEditValue = openKey.GetValue("")
+                    Catch ex As UnauthorizedAccessException
+                        MessageBox.Show("UnauthorizedAccessException", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        End
+                    End Try
+                End If
+            End Using
+            If Not pinEditValue = "" Then
+                Using openKey As RegistryKey = rkReg.OpenSubKey(strKey, True)
+                    Try
+                        ' Clean old registry for the ScreenRes path and only if Yes is choosen it is regenerated.
+                        Try
+                            openKey.DeleteSubKeyTree("B2SServer")
+                        Catch ex As ArgumentException
+
+                        End Try
+                        If (dialogResult = DialogResult.Yes) Then
+
+                            'Add VPX Pov export 
+                            'Dim exportPOVKey As RegistryKey = openKey.CreateSubKey("export POV").CreateSubKey("command")
+                            'exportPOVKey.SetValue("", pinEditValue.Replace(" -edit ", " -Pov "))
+
+                            'Add VBS export 
+                            'Dim exportVBSKey As RegistryKey = openKey.CreateSubKey("export VBS").CreateSubKey("command")
+                            'exportVBSKey.SetValue("", pinEditValue.Replace(" -edit ", " -ExtractVBS "))
+
+                            Dim sFiles() As String = Directory.GetFiles("ScreenResTemplates", "*.res")
+                            'And then add it in a Label in the way you want
+                            If sFiles.Length > 0 Then
+                                Dim vpxtoolstoplevel As RegistryKey = openKey.CreateSubKey("B2SServer")
+                                vpxtoolstoplevel.SetValue("MUIVerb", "B2S Server copy Screenres template")
+                                vpxtoolstoplevel.SetValue("subcommands", "")
+                                vpxtoolstoplevel.SetValue("Icon", """" & IO.Path.Combine(Path.GetDirectoryName(Application.ExecutablePath()), "B2SBackglassServerEXE.exe") & """")
+                                For Each resFileName As String In sFiles
+                                    '           "D:\vPinball\VisualPinball\B2SServer\ScreenResTemplates.cmd" "ScreenResTemplates\Full Screen.res" "%L"
+                                    Dim shellText As String = """" + IO.Path.Combine(Path.GetDirectoryName(Application.ExecutablePath()), "ScreenResTemplates.cmd") + """ """ + resFileName + """ ""%L"""
+                                    vpxtoolstoplevel.CreateSubKey("shell").CreateSubKey(Path.GetFileNameWithoutExtension(resFileName)).CreateSubKey("command").SetValue("", shellText)
+                                Next
+                                vpxtoolstoplevel.Close()
+                            End If
+                        End If
+                    Catch ex As UnauthorizedAccessException
+                            MessageBox.Show("UnauthorizedAccessException", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End Try
+                End Using
             End If
         End If
 
-        ' check whether everything is fine
-        If String.IsNullOrEmpty(basepath) Then
-            MessageBox.Show("Error, no basepath found.")
-        ElseIf String.IsNullOrEmpty(regasmpath) Then
-            MessageBox.Show("Error, no regasmpath found.")
-        Else
-            ' do the register operation
-            ShellAndWait(regasmpath)
-            ShellAndWait(regasmpath.Replace("\Framework\", "\Framework64\"))
-        End If
 
-        CheckB2SServer(True)
-
-        'End If
-
-        'Me.Close()
         End
 
     End Sub
 
-    Private Function CheckB2SServer(ByVal showerrormessage As Boolean) As Boolean
+    Private Function CheckB2SServer(ByVal showmessages As Boolean) As Boolean
 
-        Dim identity = WindowsIdentity.GetCurrent()
-        Dim principal = New WindowsPrincipal(identity)
-        Dim isElevated As Boolean = principal.IsInRole(WindowsBuiltInRole.Administrator)
+        'Dim identity = WindowsIdentity.GetCurrent()
+        'Dim principal = New WindowsPrincipal(identity)
+        'Dim isElevated As Boolean = principal.IsInRole(WindowsBuiltInRole.Administrator)
 
         Dim ret As Boolean = True
 
@@ -84,8 +143,8 @@ Public Class formBackglassServerRegApp
             ret = False
         End Try
         If Not err Then
-            MessageBox.Show("Everything is fine, the 'B2S backglass server' is registered.", My.Application.Info.AssemblyName, MessageBoxButtons.OK, MessageBoxIcon.Information)
-        ElseIf showerrormessage Then
+            If showmessages Then MessageBox.Show("Everything is fine, the 'B2S backglass server' is registered.", My.Application.Info.AssemblyName, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        ElseIf showmessages Then
             MessageBox.Show("Oops, the 'B2S backglass server' is NOT registered. Have you started this app as 'Administrator'?" & vbCrLf & vbCrLf & "(" & errmessage & ")", My.Application.Info.AssemblyName, MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
 
@@ -110,8 +169,9 @@ Public Class formBackglassServerRegApp
             ' free resources associated with this process
             process.Close()
             process.Dispose()
-            process = Nothing
-
+            'process = Nothing
+        Else
+            MessageBox.Show("Oops, the regasm.exe could not be found. Have you started this app as 'Administrator'?", My.Application.Info.AssemblyName, MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
 
     End Sub
