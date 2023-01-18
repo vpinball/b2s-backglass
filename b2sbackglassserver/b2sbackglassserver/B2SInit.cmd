59d7dd78d9ed86048d7da30d01e4a677de52b99a
Richard Ludwig <Richard.Ludwig+gh@gmail.com>
2023-01-18 17:34:38 +0100

    Update to B2SInit.cmd, to cut also from the "B2S DMD"

--- b2sbackglassserver/b2sbackglassserver/B2SInit.cmd
+++ b2sbackglassserver/b2sbackglassserver/B2SInit.cmd
@@ -2,4 +2,4 @@
 echo %0 %* > "%~dp0\B2SWindowPunch.log"
 timeout /t 10 /nobreak > NUL
 REM Cut holes in the "B2S Backglass Server" form using "Virtual DMD" and all "PUPSCREEN" forms as regular expressions
-"%~dp0\B2SWindowPunch.exe" "^B2S Backglass Server$" "^Virtual DMD$|^PUPSCREEN[0-9]+$" >> "%~dp0\B2SWindowPunch.log"
+"%~dp0\B2SWindowPunch.exe" "^B2S Backglass Server$|^B2S DMD$" "^Virtual DMD$|^PUPSCREEN[0-9]+$" >> "%~dp0\B2SWindowPunch.log"
