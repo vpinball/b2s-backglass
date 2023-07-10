5e5ae8826d327ceb0bc555ed924773279345b481
Jarr3 <Richard.Ludwig+gh@gmail.com>
2023-07-10 08:02:14 +0200

    #60 Also handle ampersand in filename for B2SWindowPunch

--- b2sbackglassserver/b2sbackglassserver/B2SInit.cmd
+++ b2sbackglassserver/b2sbackglassserver/B2SInit.cmd
@@ -2,7 +2,7 @@
 echo %0 %* > "%~dp0B2SWindowPunch.log"
 setlocal EnableDelayedExpansion
 
-set resfile=%cd%\%~1.res
+set resfile="%cd%\%~1.res"
 set GameName=%~2
 
 REM Cut holes in the destination "B2S Backglass Server" & "B2S DMD" forms
@@ -15,7 +15,7 @@ REM If started from explorer, remove all Windowpunch by searching for something 
 if ["%GameName%"] == [""] set "cutter=^B2SWindowPunchDeactivated$"
 
 REM Check if there is any table specific settings for B2SWindowPunch
-if exist "%resfile%" for /f "usebackq eol=# tokens=1,2 delims==" %%G in (`findstr "=" "%resfile%"`) do set "%%G=%%H"
+if exist "%resfile:~1,-1%" for /f "usebackq eol=# tokens=1,2 delims==" %%G in (`findstr "=" "%resfile:~1,-1%"`) do set "%%G=%%H"
 
 echo B2SWindowPunch = "!B2SWindowPunch!" >> "%~dp0B2SWindowPunch.log"
 echo cutter = "!cutter!" >> "%~dp0B2SWindowPunch.log"
