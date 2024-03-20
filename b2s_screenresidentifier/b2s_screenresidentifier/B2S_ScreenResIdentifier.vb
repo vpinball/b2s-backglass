6f13aef581b7a79104aa7ca590ff95b62cb72e37
Jarr3 <Richard.Ludwig+gh@gmail.com>
2024-03-20 07:49:06 +0100

    Revert "b2s tools should not allow to be run as admin #75"
    This reverts commit bfd7f4dd4610ffd7b6345ad8938e88d550753c09.

--- b2s_screenresidentifier/b2s_screenresidentifier/B2S_ScreenResIdentifier.vb
+++ b2s_screenresidentifier/b2s_screenresidentifier/B2S_ScreenResIdentifier.vb
@@ -1,25 +1,13 @@
 Imports System
 Imports System.Drawing
 Imports System.IO
-Imports System.Security.Principal
 Imports System.Windows.Forms
 Imports Microsoft.Win32
-Imports MessageBox = System.Windows.MessageBox
 
 Module B2S_ScreenResIdentifier
     Sub Main()
-        If IsAdmin() Then
-            MessageBox.Show("You should not start this as Administrator!", My.Resources.AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Stop)
-            Exit Sub
-        End If
-
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
         Application.Run(New formPlayfield())
     End Sub
-    Private Function IsAdmin() As Boolean
-        Dim identity As WindowsIdentity = WindowsIdentity.GetCurrent()
-        Dim principal As New WindowsPrincipal(identity)
-        Return principal.IsInRole(WindowsBuiltInRole.Administrator)
-    End Function
 End Module
