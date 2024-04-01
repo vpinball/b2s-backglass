580ca42a82a1d11973a8ce0ef5298aac3cef3460
Jarr3 <Richard.Ludwig+gh@gmail.com>
2024-04-01 12:38:03 +0200

    Update B2S_ScreenResIdentifier.vb

--- b2s_screenresidentifier/b2s_screenresidentifier/B2S_ScreenResIdentifier.vb
+++ b2s_screenresidentifier/b2s_screenresidentifier/B2S_ScreenResIdentifier.vb
@@ -6,12 +6,6 @@ Imports Microsoft.Win32
 
 Module B2S_ScreenResIdentifier
     Sub Main()
-        If IsAdmin() Then
-            MessageBox.Show("You should not start this as Administrator!", My.Resources.AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Stop)
-            ' Too much noice here if we would quit out...
-            'Exit Sub
-        End If
-
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
         Application.Run(New formPlayfield())
