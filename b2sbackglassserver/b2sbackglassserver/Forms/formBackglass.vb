ca2e764e195d7088ecdff8f7d2531773994d0677
Jarr3 <Richard.Ludwig+gh@gmail.com>
2024-05-09 20:26:18 +0200

    Revert mergeBulbs

--- b2sbackglassserver/b2sbackglassserver/Forms/formBackglass.vb
+++ b2sbackglassserver/b2sbackglassserver/Forms/formBackglass.vb
@@ -773,7 +773,11 @@ Public Class formBackglass
 
                 ' get top node
                 Dim topnode As Xml.XmlElement = XML.SelectSingleNode("DirectB2SData")
-                Dim mergeBulbs As Boolean = topnode.SelectSingleNode("MergeBulbs") IsNot Nothing AndAlso topnode.SelectSingleNode("MergeBulbs").Attributes("Value").InnerText <> "0"
+                Dim mergeBulbs As Boolean = True
+
+                If topnode.SelectSingleNode("MergeBulbs") IsNot Nothing Then
+                    mergeBulbs = topnode.SelectSingleNode("MergeBulbs").Attributes("Value").InnerText <> "0"
+                End If
 
                 ' clear all data
                 B2SData.ClearAll(True)
