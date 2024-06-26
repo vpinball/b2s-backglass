ffa10387700b721f9ad1709fa23c4a3ef71e60a9
Jarr3 <Richard.Ludwig+gh@gmail.com>
2024-06-26 18:25:59 +0200

    2.5.0: Finally a merge of the source code of B2SBackglassServer & B2SBackglassServerEXE into one. Should not be noticed while running, but huge for development.

--- b2sbackglassserverregisterapp/b2sbackglassserverregisterapp/My Project/AssemblyInfo.vb
+++ b2sbackglassserverregisterapp/b2sbackglassserverregisterapp/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.0.0")> 
-<Assembly: AssemblyFileVersion("3.0.0")> 
-<Assembly: AssemblyInformationalVersion("3.0.0")> 
+<Assembly: AssemblyVersion("2.5.0")> 
+<Assembly: AssemblyFileVersion("2.5.0")> 
+<Assembly: AssemblyInformationalVersion("2.5.0")> 
