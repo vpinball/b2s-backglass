49453e7e25a2be7d4ea8cb7bfee57553857b86da
Jarr3 <Richard.Ludwig+gh@gmail.com>
2024-06-28 08:00:27 +0200

    A bit more merged / Split

--- b2sbackglassserver/b2sbackglassserver/Classes/B2SBackglassServerEXE.vb
+++ b2sbackglassserver/b2sbackglassserver/Classes/B2SBackglassServerEXE.vb
@@ -7,17 +7,8 @@ Imports Microsoft.Win32
 
 Module B2SBackglassServerEXE
     Sub Main()
-        'If IsAdmin() Then
-        '    MessageBox.Show("You should not start this as Administrator!", My.Resources.AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Stop)
-        '    Exit Sub
-        'End If
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
         Application.Run(New formBackglass())
     End Sub
-    Private Function IsAdmin() As Boolean
-        Dim identity As WindowsIdentity = WindowsIdentity.GetCurrent()
-        Dim principal As New WindowsPrincipal(identity)
-        Return principal.IsInRole(WindowsBuiltInRole.Administrator)
-    End Function
 End Module
