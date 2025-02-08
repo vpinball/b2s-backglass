cc49b771ce120edb67d0f758577f3ce1bc631549
Jarr3 <Richard.Ludwig+gh@gmail.com>
2025-02-08 22:30:53 +0100

    #65 Add DPI awareness to B2S_ScreenResIdentifier and B2SBackglassServerEXE Was lost in last merge operation

--- b2sbackglassserver/b2sbackglassserver/My Project/AssemblyInfo.vb
+++ b2sbackglassserver/b2sbackglassserver/My Project/AssemblyInfo.vb
@@ -33,6 +33,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.5.0")> 
-<Assembly: AssemblyFileVersion("2.5.0")> 
-<Assembly: AssemblyInformationalVersion("2.5.0")> 
+<Assembly: AssemblyVersion("2.1.3")> 
+<Assembly: AssemblyFileVersion("2.1.3")> 
+<Assembly: AssemblyInformationalVersion("2.1.3")> 
