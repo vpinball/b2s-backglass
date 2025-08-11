b163948bec138392d59a8a63f0a3bd1acbfb8afe
Jarr3 <Richard.Ludwig+gh@gmail.com>
2025-08-11 17:41:27 +0200

    #146 Add updated version of B2S_SetUp working with comment lines, new format and so on

---
+++ B2S_SetUp/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("B2S_SetUp")]
+[assembly: AssemblyDescription("B2S ScreenRes Editor")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("B2S_SetUp")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5b5c7e7-7c5d-4c8b-8c2a-5c7b2c0d0c0d")]
+
+[assembly: AssemblyVersion("2.1.4.999")]
+[assembly: AssemblyFileVersion("2.1.4.999")]
+[assembly: AssemblyInformationalVersion("2.1.4.999")]
